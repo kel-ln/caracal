af1437798e6177b7887296a1386350c65e890aaf
Kel <emiralievlenur@gmail.com>
2020-05-03 12:51:05 +0500

    f

--- lib/caracal/core/models/image_model.rb
+++ lib/caracal/core/models/image_model.rb
@@ -4,25 +4,27 @@ require 'caracal/core/models/base_model'
 module Caracal
   module Core
     module Models
-      
+
       # This class handles block options passed to the img method.
       #
       class ImageModel < BaseModel
-        
+
         #-------------------------------------------------------------
         # Configuration
         #-------------------------------------------------------------
-        
+
         # constants
         const_set(:DEFAULT_IMAGE_PPI,     72)       # pixels per inch
         const_set(:DEFAULT_IMAGE_WIDTH,   0)        # units in pixels. (will cause error)
         const_set(:DEFAULT_IMAGE_HEIGHT,  0)        # units in pixels. (will cause error)
-        const_set(:DEFAULT_IMAGE_ALIGN,   :left) 
-        const_set(:DEFAULT_IMAGE_TOP,     8)        # units in pixels. 
-        const_set(:DEFAULT_IMAGE_BOTTOM,  8)        # units in pixels. 
-        const_set(:DEFAULT_IMAGE_LEFT,    8)        # units in pixels. 
-        const_set(:DEFAULT_IMAGE_RIGHT,   8)        # units in pixels. 
-        
+        const_set(:DEFAULT_IMAGE_ALIGN,   :left)
+        const_set(:DEFAULT_IMAGE_TOP,     8)        # units in pixels.
+        const_set(:DEFAULT_IMAGE_BOTTOM,  8)        # units in pixels.
+        const_set(:DEFAULT_IMAGE_LEFT,    8)        # units in pixels.
+        const_set(:DEFAULT_IMAGE_RIGHT,   8)        # units in pixels.
+        const_set(:DEFAULT_IMAGE_POS_X,   0)
+        const_set(:DEFAULT_IMAGE_POS_Y,   0)
+
         # accessors
         attr_reader :image_url
         attr_reader :image_data
@@ -34,8 +36,10 @@ module Caracal
         attr_reader :image_bottom
         attr_reader :image_left
         attr_reader :image_right
-        
-        
+        attr_reader :pos_x
+        attr_reader :pos_y
+
+
         # initialization
         def initialize(options={}, &block)
           @image_ppi    = DEFAULT_IMAGE_PPI
@@ -46,88 +50,102 @@ module Caracal
           @image_bottom = DEFAULT_IMAGE_BOTTOM
           @image_left   = DEFAULT_IMAGE_LEFT
           @image_right  = DEFAULT_IMAGE_RIGHT
-          
+          @image_pos_x  = DEFAULT_IMAGE_POS_X
+          @image_pos_y  = DEFAULT_IMAGE_POS_Y
+
           super options, &block
         end
-        
-        
+
+
         #-------------------------------------------------------------
         # Public Methods
         #-------------------------------------------------------------
-        
+
         #=============== GETTERS ==============================
-        
+
         [:width, :height].each do |m|
           define_method "formatted_#{ m }" do
             value = send("image_#{ m }")
             pixels_to_emus(value, image_ppi)
           end
         end
-        
+
         [:top, :bottom, :left, :right].each do |m|
           define_method "formatted_#{ m }" do
             value = send("image_#{ m }")
             pixels_to_emus(value, 72)
           end
         end
-        
+
+        [:pos_x, :pos_y].each do |m|
+          define_method "formatted_#{ m }" do
+            value = send("image_#{ m }")
+            pixels_to_emus(value, 72)
+          end
+        end
+
         def relationship_target
           image_url || image_data
         end
-        
-        
+
+
         #=============== SETTERS ==============================
-        
+
         # integers
         [:ppi, :width, :height, :top, :bottom, :left, :right].each do |m|
           define_method "#{ m }" do |value|
             instance_variable_set("@image_#{ m }", value.to_i)
           end
         end
-        
+
         # strings
         [:data, :url].each do |m|
           define_method "#{ m }" do |value|
             instance_variable_set("@image_#{ m }", value.to_s)
           end
         end
-        
+
         # symbols
         [:align].each do |m|
           define_method "#{ m }" do |value|
             instance_variable_set("@image_#{ m }", value.to_s.to_sym)
           end
-        end        
-        
-        
+        end
+
+        [:pos_x, :pos_y].each do |m|
+          define_method "#{ m }" do |value|
+            instance_variable_set("@image_#{ m }", valiue.to_s.to_sym)
+          end
+        end
+
         #=============== VALIDATION ==============================
-        
+
         def valid?
           dims = [:ppi, :width, :height, :top, :bottom, :left, :right].map { |m| send("image_#{ m }") }
           dims.all? { |d| d > 0 }
         end
-        
-        
-        
+
+
+
         #-------------------------------------------------------------
         # Private Methods
         #-------------------------------------------------------------
         private
-        
+
         def option_keys
-          [:url, :width, :height, :align, :top, :bottom, :left, :right]
+          [:url, :width, :height, :align, :top, :bottom, :left, :right, :pos_x, :pos_y]
         end
-        
+
         def pixels_to_emus(value, ppi)
           pixels        = value.to_i
           inches        = pixels / ppi.to_f
           emus_per_inch = 914400
-        
-          emus = (inches * emus_per_inch).to_i 
+
+          emus = (inches * emus_per_inch).to_i
         end
-        
+
       end
-      
+
     end
   end
 end
